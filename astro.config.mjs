cd5a5094124695e9302c20fa1c38b6c8ef7125fc
Cal <calpnicklin@gmail.com>
2024-11-20 21:43:21 +0000

    adds mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,11 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMermaidjs]
+  },
   output: 'server',
   adapter: vercel()
 });
